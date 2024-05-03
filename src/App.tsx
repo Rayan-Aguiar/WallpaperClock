8c8f0f20087c60c5d16a397351cd4eadf2935d7e
Rayan <rayansiqueira@gmail.com>
2024-05-03 13:10:15 -0300

    finish wallpaper code

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
-export default function App() { 
+import { useEffect, useState } from "react"
+export default function Home() {
+  const [time, setTime] = useState(getTime());
+
+  function getTime() {
+    const now = new Date();
+    return{
+      hour: now.getHours(),
+      minute: now.getMinutes(),
+      day: now.getDate(),
+      mouth: now.toLocaleString('default', { month: 'long' }),
+      year: now.getFullYear(),
+    }
+  }
+
+  useEffect(()=>{
+    const interval = setInterval(()=>{
+      setTime(getTime());
+    }, 1000);
+    return ()=>{ clearInterval(interval);}
+  }, []);
+
+  if (typeof window === 'undefined') {
+    return null; 
+  }
+
   return (
     <>
       <div className="bg-zinc-950 w-screen h-screen flex flex-col text-white justify-center items-center ">
-        <div className="text-4xl flex flex-col gap-3">
-          <div className="flex gap-2">
-            <span className="text-violet-400">const</span>
-            <span>clock = <span className="text-amber-500">{'{'}</span>  </span>          
-          </div>
-          <div className="flex flex-col relative left-2">
-            <span>hour: <span className="text-green-500 ">11</span>,</span>
-            <span>minute: <span className="text-green-500 ">50</span>,</span>
-            <span>day: <span className="text-green-500 ">03</span>,</span>
-            <span>mouth: <span className="text-green-500 ">"April"</span>,</span>
-            <span>year: <span className="text-green-500 ">"2024"</span></span>
-          </div>
-          <div className="flex gap-2">
-            <span className="text-amber-500">{'}'} </span>          
+          <div className="text-4xl flex flex-col gap-3">
+            <div className="flex gap-2">
+              <span className="text-violet-400">const</span>
+              <span>clock = <span className="text-amber-500">{'{'}</span>  </span>          
+            </div>
+            <div className="flex flex-col relative left-10">
+              <span>hour: <span className="text-green-500 ">{time.hour}</span>,</span>
+              <span>minute: <span className="text-green-500 ">{time.minute}</span>,</span>
+              <span>day: <span className="text-green-500 ">{time.day}</span>,</span>
+              <span>mouth: <span className="text-green-500 ">{time.mouth}</span>,</span>
+              <span>year: <span className="text-green-500 ">{time.year}</span></span>
+            </div>
+            <div className="flex gap-2">
+              <span className="text-amber-500">{'}'} </span>          
+            </div>
           </div>
         </div>
-      </div>
     </>
   )
 }
-
-
